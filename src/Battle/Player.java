498dd6e84235df6b2e1accc89c8b764ac2edf00d
Eduardo P <edtvports@gmail.com>
2023-06-29 13:52:49 -0300

    default implementation for hasItem()

--- src/Battle/Player.java
+++ src/Battle/Player.java
@@ -1,13 +1,25 @@
 package Battle;
 
+import java.util.List;
+
 public interface Player {
     void equipItem(EquipableItems item) throws IllegalStateException;
+
+    default boolean hasItem(EquipableItems item, List<EquipableItems> equipableItemsList) {
+        for (EquipableItems listedItem : equipableItemsList)
+            if (listedItem.equals(item))
+                return true;
+        return false;
+    }
 
     void addItem(ConsumableItems item) throws IllegalStateException;
 
     void useItem(ConsumableItems item) throws IllegalStateException;
 
-    boolean hasItem(EquipableItems item);
-
-    boolean hasItem(ConsumableItems item);
+    default boolean hasItem(ConsumableItems item, List<ConsumableItems> consumableItemsList) {
+        for (ConsumableItems listedItem : consumableItemsList)
+            if (listedItem.equals(item))
+                return true;
+        return false;
+    }
 }

59bb00352c9dd1399bbbbd2753b53b7a81ffcc63
Eduardo P <edtvports@gmail.com>
2023-07-02 10:15:20 -0300

    Corrected mistakes

--- src/Battle/Abilities.java
+++ src/Battle/Abilities.java
@@ -1,9 +1,9 @@
 package Battle;
 
 public enum Abilities {
-    ATTACK("Ataque Básico", "Apenas um ataque básico, o que você esperava?", 1, 0);
-    ATTACK("Graça dos Doom", "Aria é capaz de controlar orbes energizadas. Como seu poder está diretamente ligado ao estado de sua mente, há um desgaste mental, ocasionando em perda de sanidade.", 2, 1); 
-    ATTACK("EGO", "Damien é capaz de controlar EGO, um sentimento selado capaz de aniquilar seus inimigos e, à medida em que utiliza de seu poder, perde sanidade.", 2, 1); 
+    ATTACK("Ataque Básico", "Apenas um ataque básico.", 1, 0),
+    GRACE("Graça dos Doom", "Capacidade de controlar orbes energizadas. Esse poder está diretamente ligado ao estado de sua mente, há um desgaste mental, ocasionando perda de sanidade.", 2, 1),
+    EGO("Ego", "Um sentimento selado capaz de aniquilar seus inimigos e, à medida em que o poder é utilizado, perde-se sanidade.", 2, 1);
     
     public final String NAME, DESCRIPTION;
     public final Integer DAMAGE, COST;

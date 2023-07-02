cfc29d833d38ea34a8e74fddde93d460dfb6f8e0
Abece <miguelbjjgames@gmail.com>
2023-07-02 12:40:52 -0300

    Update Abilities.java

--- src/Battle/Abilities.java
+++ src/Battle/Abilities.java
@@ -3,10 +3,10 @@ package Battle;
 public enum Abilities {
     ATTACK("Ataque Básico", "Apenas um ataque básico.", 1, 0),
     GRACE("Graça dos Doom", "Capacidade de controlar orbes energizadas. Há um desgaste mental nas manipulações, ocasionando perda de sanidade.", 2, 1),
-    EGO("Ego", "Um sentimento selado capaz de aniquilar seus inimigos e à medida em que o poder é utilizado, perde-se sanidade.", 2, 1),
-    OBSTACLE1("Thorffy, o Guardião de Sétiah", "Thorffy utiliza de seus gritos para aterrorizar seus inimigos. Toda vez que grita, seu alvo acaba perdendo sanidade.", 2, 2),
-    OBSTACLE2("Zigg, o Lacaio de Graf", "Zigg encontra os medos mais profundos de suas vítimas para fazê-las se renderem. Para cada ataque de Zigg, seu alvo acaba perdendo sanidade.", 3, 3),
-    OBSTACLE3("Lorde Graf", "Lorde Graf utiliza de manipulações para acabar com seus oponentes. Cada ataque mental seu diminui a sanidade de seu alvo.", 4, 4);
+    EGO("Ego", "Um sentimento selado capaz de aniquilar seus inimigos e, à medida em que o poder é utilizado, perde-se sanidade.", 2, 1),
+    SHOUT("Thorffy, o Guardião de Sétiah", "Thorffy utiliza de seus gritos para aterrorizar seus inimigos.", 2, 2),
+    FEAR("Zigg, o Lacaio de Graf", "Zigg encontra os medos mais profundos de suas vítimas para fazê-las se renderem.", 3, 3),
+    MANIPULATION("Lorde Graf", "Lorde Graf utiliza de manipulações para acabar com seus oponentes.", 4, 4);
 
     public final String NAME, DESCRIPTION;
     public final Integer DAMAGE, COST;

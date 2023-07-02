b0527c93d2a1b8df4bfc62470b373aaa03a6cee9
Eduardo P <edtvports@gmail.com>
2023-07-02 10:28:39 -0300

    Reduced ability's descriptions

--- src/Battle/Abilities.java
+++ src/Battle/Abilities.java
@@ -2,8 +2,8 @@ package Battle;
 
 public enum Abilities {
     ATTACK("Ataque Básico", "Apenas um ataque básico.", 1, 0),
-    GRACE("Graça dos Doom", "Capacidade de controlar orbes energizadas. Esse poder está diretamente ligado ao estado de sua mente, há um desgaste mental, ocasionando perda de sanidade.", 2, 1),
-    EGO("Ego", "Um sentimento selado capaz de aniquilar seus inimigos e, à medida em que o poder é utilizado, perde-se sanidade.", 2, 1);
+    GRACE("Graça dos Doom", "Capacidade de controlar orbes energizadas. Há um desgaste mental nas manipulações, ocasionando perda de sanidade.", 2, 1),
+    EGO("Ego", "Um sentimento selado capaz de aniquilar seus inimigos e à medida em que o poder é utilizado, perde-se sanidade.", 2, 1);
     
     public final String NAME, DESCRIPTION;
     public final Integer DAMAGE, COST;

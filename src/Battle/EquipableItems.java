a07bc1f6e57dc844cea428fc05d32f319272c238
Abece <miguelbjjgames@gmail.com>
2023-07-02 12:48:55 -0300

    Update EquipableItems.java

--- src/Battle/EquipableItems.java
+++ src/Battle/EquipableItems.java
@@ -1,7 +1,11 @@
 package Battle;
 
 public enum EquipableItems {
-    SHIELD("Escudo", "", new Attributes(0, 2, 0));
+    ARMOR("Armadura Brilhante", "Uma ótima opção para se proteger de intensos ataques.", new Attributes(0, 4, 0)),
+    BOOT("Bota de Velocidade", "Uma bota capaz de te tornar... mais rápido?", new Attributes(0, 2, 0)),
+    GLOVE("Luvas Brancas", "Quando vestida, torna seu usuário mais forte magicamente.", new Attributes(2, 1, 0)),
+    SHIELD("Escudo Reforçado", "Você sabia que existem apenas 100 unidades desse escudo no mundo todo? Ah, e é capaz de te proteger!", new Attributes(0, 2, 0)),
+    THORN("Capacete Espinhoso", "Dê cabeçadas para machucar seus oponentes.", new Attributes(1, 2, 0));
 
     public final String NAME, DESCRIPTION;
     public final Attributes MODIFIERS;

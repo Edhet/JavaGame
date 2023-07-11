5eb4155252430ca62ec1639e37b408bac821ae88
Abece <miguelbjjgames@gmail.com>
2023-07-11 15:15:49 -0300

    Changing Item Values

--- src/Model/Constants/EquipableItems.java
+++ src/Model/Constants/EquipableItems.java
@@ -3,11 +3,11 @@ package Model.Constants;
 import Model.Attributes;
 
 public enum EquipableItems {
-    ARMOR("Armadura Brilhante", "Uma ótima opção para se proteger de intensos ataques.", new Attributes(0, 4, 0)),
-    BOOT("Bota de Velocidade", "Uma bota capaz de te tornar... mais rápido?", new Attributes(0, 1, 5)),
-    GLOVE("Luvas Brancas", "Quando vestida, torna seu usuário mais forte magicamente.", new Attributes(3, 1, 0)),
-    SHIELD("Escudo Reforçado", "Um escudo duas vezes melhor!", new Attributes(1, 3, 0)),
-    THORN("Capacete Espinhoso", "Dê cabeçadas para machucar seus oponentes.", new Attributes(2, 2, 0));
+    ARMOR("Armadura Brilhante", "Uma ótima opção para se proteger de intensos ataques.", new Attributes(0, 2, 0)),
+    BOOT("Bota de Velocidade", "Uma bota capaz de te tornar... mais rápido?", new Attributes(0, 1, 2)),
+    GLOVE("Luvas Brancas", "Quando vestida, torna seu usuário mais forte magicamente.", new Attributes(2, 1, 0)),
+    SHIELD("Escudo Reforçado", "Um escudo duas vezes melhor!", new Attributes(1, 2, 0)),
+    THORN("Capacete Espinhoso", "Dê cabeçadas para machucar seus oponentes.", new Attributes(1, 1, 0));
 
     public final String NAME, DESCRIPTION;
     public final Attributes MODIFIERS;

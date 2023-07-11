01b8944d6870a7f3c5ef7ba33b925e487cab96e3
Abece <miguelbjjgames@gmail.com>
2023-07-11 04:00:56 -0300

    Change Description

--- src/Model/Constants/EquipableItems.java
+++ src/Model/Constants/EquipableItems.java
@@ -4,10 +4,10 @@ import Model.Attributes;
 
 public enum EquipableItems {
     ARMOR("Armadura Brilhante", "Uma ótima opção para se proteger de intensos ataques.", new Attributes(0, 4, 0)),
-    BOOT("Bota de Velocidade", "Uma bota capaz de te tornar... mais rápido?", new Attributes(0, 2, 0)),
-    GLOVE("Luvas Brancas", "Quando vestida, torna seu usuário mais forte magicamente.", new Attributes(2, 1, 0)),
-    SHIELD("Escudo Reforçado", "Você sabia que existem apenas 100 unidades desse escudo no mundo todo? Ah, e é capaz de te proteger!", new Attributes(0, 2, 0)),
-    THORN("Capacete Espinhoso", "Dê cabeçadas para machucar seus oponentes.", new Attributes(1, 2, 0));
+    BOOT("Bota de Velocidade", "Uma bota capaz de te tornar... mais rápido?", new Attributes(0, 1, 5)),
+    GLOVE("Luvas Brancas", "Quando vestida, torna seu usuário mais forte magicamente.", new Attributes(3, 1, 0)),
+    SHIELD("Escudo Reforçado", "Um escudo duas vezes melhor!", new Attributes(1, 3, 0)),
+    THORN("Capacete Espinhoso", "Dê cabeçadas para machucar seus oponentes.", new Attributes(2, 2, 0));
 
     public final String NAME, DESCRIPTION;
     public final Attributes MODIFIERS;
